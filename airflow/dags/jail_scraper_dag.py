86da2c8b01c9b6fec973b32707ddf43415a49484
Ryan Carroll <raec901@gmail.com>
2020-05-03 13:35:12 -0500

    feat: Import custom functionality from packages instead of local submodule

--- airflow/dags/jail_scraper_dag.py
+++ airflow/dags/jail_scraper_dag.py
@@ -1,67 +1,80 @@
+# standard library
 import datetime as dt
+from datetime import datetime
 import os
 import math
 import csv
 import logging
 
+# external dependencies
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.models import Variable
 
+# custom packages
+from csci_utils.airflow import PythonIdempatomicFileOperator, requires
 from jail_scraper.airflow_scraper import main
-from datetime import datetime
-
 from odyssey_scraper.smartsearch import SmartSearchScraper
 
-SCRAPE_DIR = "data/scrapes/" + datetime.today().strftime("%m-%d-%Y") + "/"
+SCRAPE_ROOT = "data/scrapes/" + datetime.today().strftime("%m-%d-%Y") + "/"
 
-def check_jail_profiles():
+def scrape_jail(output_path, test):
+    main(scrape_dir=output_path, test = test)
+    return output_path
 
-    with open(SCRAPE_DIR + "people.csv", "r", newline="") as fout:
+def check_jail_profiles(output_path, **kwargs):
+
+    # load filepaths from required task
+    reqs = requires('scrape_jail', **kwargs)
+    logging.info('Requirements:', str(reqs))
+
+    # get info for people in jail (which is stored in people.csv in dir created by 'scrape_jail')
+    with open(reqs['people'], "r", newline="") as fout:
         data = list(csv.reader(fout))
-
     logging.info("opened people.csv")
 
-    # data = check(data)
-
+    # user decides scrapes_per_worker depending on personal preference and number of scrapers
     scrapes_per_worker = int(Variable.get("scrapes_per_worker", default_var=3))
     logging.info("scrapes_per_worker = " + str(scrapes_per_worker))
 
+    # how many people need their profiles scraped
     num_people_to_scrape = len(data)
     logging.info("num_people_to_scrape = " + str(num_people_to_scrape))
 
+    # this determines how many tasks we create to do the scraping (which allows it to be done in
+    # parallel when deployed)
     num_tasks = math.ceil(num_people_to_scrape/scrapes_per_worker)
     logging.info("num_tasks = " + str(num_tasks))
 
-    # MAKE THIS ATOMIC
+    # split big list of people into 'to do lists" for each of the workers. If worker fails, it can
+    # be re-run and pull in exact same people
     for x in range(num_tasks):
         chunk = data[x*scrapes_per_worker+1:(x+1)*scrapes_per_worker+1]
-        out_path = os.path.join(SCRAPE_DIR, f"todo_{x}.csv")
+        out_path = os.path.join(output_path, f"todo_{x}.csv")
         with open(out_path, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(chunk)
         logging.info(f"wrote todo_{x}")
 
-    # set variable in Airflow to use in constructing dynamic DAG
-    os.system('airflow variables --set num_odyssey_scraping_tasks ' + str(num_tasks))
+    # set variable in Airflow (stored in meta-db) to use in constructing dynamic DAG later
+    Variable.set('num_odyssey_scraping_tasks', str(num_tasks))
+    return 'Complete'
 
+def scrape_odyssey(index, output_path, **kwargs):
 
-def my_callable(index):
-
-    out_path = SCRAPE_DIR + f"/worker_{index}/cases.csv"
-
-    # if already exists, then skip
-    if os.path.exists(out_path):
-        return 'Previously Completed'
-
-    # if it doesn't exist, scrape
     logging.info(f'running scraper {index}')
-    with open(SCRAPE_DIR + f"todo_{index}.csv", "r", newline="") as fout:
+
+    out_path = output_path
+    reqs = requires('check_profiles', **kwargs)
+
+    # read in "to do list"
+    with open(reqs[f'todo_{index}'], "r", newline="") as fout:
         data = list(csv.reader(fout))
 
-
+    # log in information for Odyssey Criminal Justice Portal (Shelby County, TN)
     odyssey_user = os.environ['ODYSSEY_USER']
     odyssey_pwd = os.environ['ODYSSEY_PASS']
+    # scraper that I developed for this system
     scr = SmartSearchScraper(odyssey_user, odyssey_pwd)
 
     total_cases = []
@@ -71,22 +84,19 @@ def my_callable(index):
         dob = person[2]
         justice_history = scr.query_name_dob(name, dob, get_rni=True)
         for case in justice_history.case_grid_list():
-            total_cases.append(case)
+            # combine all a person's cases into a list of lists (instead of a list of list of lists)
+            total_cases += case
     scr.quit()
-
-
-    logging.info('writing ' + out_path)
-    if not os.path.exists(out_path):
-        os.makedirs(os.path.dirname(out_path))
 
     with open(out_path, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(total_cases)
 
-    return 'Completed'
+    return 'Complete'
 
 
-def upload_data():
+def upload_data(**kwargs):
+    # dummy task for "uploading data"
     pass
 
 default_args = {
@@ -102,33 +112,35 @@ dag =  DAG('jail_scraper_dag',
          )
 
 
-scrape_jail = PythonOperator(task_id='scrape_jail',
-                            python_callable=main,
-                            op_kwargs={'test':True},
-                            dag = dag)
+jail_scraper = PythonIdempatomicFileOperator(task_id='scrape_jail',
+                                        python_callable=scrape_jail,
+                                        output_pattern = SCRAPE_ROOT + "jail_scrape/",
+                                        op_kwargs={'test':True},
+                                        dag = dag)
 
-check_profiles = PythonOperator(task_id='check_profiles',
-                                 python_callable=check_jail_profiles,
-                                dag = dag)
+check_profiles = PythonIdempatomicFileOperator(task_id='check_profiles',
+                                            python_callable=check_jail_profiles,
+                                            output_pattern = SCRAPE_ROOT + "to_do/",
+                                            provide_context=True,
+                                            dag = dag)
+upload = PythonOperator(task_id='upload_data',
+                        python_callable=upload_data,
+                        dag=dag)
 
-num_tasks = int(Variable.get("num_odyssey_scraping_tasks", default_var=4))
+num_tasks = int(Variable.get("num_odyssey_scraping_tasks", default_var=1))
 for i in range(num_tasks):
-    odyssey_scraper = PythonOperator(task_id='odyssey_scraper_'+str(i),
-                                    dag = dag,
-                                    python_callable=my_callable,
-                                    op_args=[i])
+    odyssey_scraper = PythonIdempatomicFileOperator(task_id='odyssey_scraper_'+str(i),
+                                                    output_pattern = SCRAPE_ROOT + "worker_{index}/cases.csv",
+                                                    dag = dag,
+                                                    python_callable=scrape_odyssey,
+                                                    provide_context=True,
+                                                    op_kwargs={'index': i})
 
     check_profiles.set_downstream(odyssey_scraper)
-    # odyssey_scraper.set_downstream(upload_task)
-
-#scrape_odyssey = PythonOperator(task_id='scrape_odyssey',
-#                             python_callable=respond)
+    odyssey_scraper.set_downstream(upload)
 
 
 
-scrape_jail >> check_profiles
 
+jail_scraper >> check_profiles
 
-
-if __name__ == "__main__":
-    check_jail_profiles()
