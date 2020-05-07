4e588bee5da647886e4ebbc157c4ebea1ffe196d
Ryan Carroll <raec901@gmail.com>
2020-05-07 17:58:49 -0500

    style: black

--- airflow/dags/jail_scraper_dag.py
+++ airflow/dags/jail_scraper_dag.py
@@ -24,8 +24,9 @@ def scrape_jail(output_path, test):
     to our normalized database schema. These files will be uploaded to the database after we check
     if they already have a profile
     """
-    scrape_jail_website(scrape_dir=output_path, test = test)
+    scrape_jail_website(scrape_dir=output_path, test=test)
     return output_path
+
 
 def check_jail_profiles(output_path, **kwargs):
     """
@@ -38,11 +39,11 @@ def check_jail_profiles(output_path, **kwargs):
     """
 
     # load filepaths from required task
-    reqs = requires('scrape_jail', **kwargs)
-    logging.info('Requirements:', str(reqs))
+    reqs = requires("scrape_jail", **kwargs)
+    logging.info("Requirements:", str(reqs))
 
     # get info for people in jail (which is stored in people.csv in dir created by 'scrape_jail')
-    with open(reqs['people'], "r", newline="") as fout:
+    with open(reqs["people"], "r", newline="") as fout:
         data = list(csv.reader(fout))
     logging.info("opened people.csv")
 
@@ -56,13 +57,13 @@ def check_jail_profiles(output_path, **kwargs):
 
     # this determines how many tasks we create to do the scraping (which allows it to be done in
     # parallel when deployed)
-    num_tasks = math.ceil(num_people_to_scrape/scrapes_per_worker)
+    num_tasks = math.ceil(num_people_to_scrape / scrapes_per_worker)
     logging.info("num_tasks = " + str(num_tasks))
 
     # split big list of people into 'to do lists" for each of the workers. If worker fails, it can
     # be re-run and pull in exact same people
     for x in range(num_tasks):
-        chunk = data[x*scrapes_per_worker+1:(x+1)*scrapes_per_worker+1]
+        chunk = data[x * scrapes_per_worker + 1 : (x + 1) * scrapes_per_worker + 1]
         out_path = os.path.join(output_path, f"todo_{x}.csv")
         with open(out_path, "w", newline="") as f:
             writer = csv.writer(f)
@@ -70,14 +71,19 @@ def check_jail_profiles(output_path, **kwargs):
         logging.info(f"wrote todo_{x}")
 
     # set variable in Airflow (stored in meta-db) to use in constructing dynamic DAG later
-    Variable.set('num_odyssey_scraping_tasks', num_tasks)
+    Variable.set("num_odyssey_scraping_tasks", num_tasks)
 
     # set variable that controls concurrency (basically concurrency proportial to percentage of scrapes up to 1000)
     max_concurrency = os.cpu_count() - 1
-    concurrency = math.ceil(max_concurrency * num_tasks/1000) if num_tasks/1000 < 1 else max_concurrency
-    Variable.set('concurrency', concurrency)
+    concurrency = (
+        math.ceil(max_concurrency * num_tasks / 1000)
+        if num_tasks / 1000 < 1
+        else max_concurrency
+    )
+    Variable.set("concurrency", concurrency)
 
-    return 'Complete'
+    return "Complete"
+
 
 def scrape_odyssey(index, output_path, **kwargs):
     """
@@ -88,18 +94,18 @@ def scrape_odyssey(index, output_path, **kwargs):
     :param index: the ID number of the worker (which corresponds to the list it reads)
     """
 
-    logging.info(f'running scraper {index}')
+    logging.info(f"running scraper {index}")
 
     out_path = output_path
-    reqs = requires('check_profiles', **kwargs)
+    reqs = requires("check_profiles", **kwargs)
 
     # read in "to do list"
-    with open(reqs[f'todo_{index}'], "r", newline="") as fout:
+    with open(reqs[f"todo_{index}"], "r", newline="") as fout:
         data = list(csv.reader(fout))
 
     # log in information for Odyssey Criminal Justice Portal (Shelby County, TN)
-    odyssey_user = os.environ['ODYSSEY_USER']
-    odyssey_pwd = os.environ['ODYSSEY_PASS']
+    odyssey_user = os.environ["ODYSSEY_USER"]
+    odyssey_pwd = os.environ["ODYSSEY_PASS"]
     # scraper that I developed for this system
     scr = SmartSearchScraper(odyssey_user, odyssey_pwd)
 
@@ -119,7 +125,7 @@ def scrape_odyssey(index, output_path, **kwargs):
         writer = csv.writer(f)
         writer.writerows(total_cases)
 
-    return 'Complete'
+    return "Complete"
 
 
 def upload_data():
@@ -128,35 +134,34 @@ def upload_data():
     """
     pass
 
+
 default_args = {
-    'owner': 'airflow',
-    'start_date': dt.datetime(2020, 4, 30, 16, 00, 00),
-    'concurrency': int(Variable.get("concurrency", default_var=1)),
-    'retries': 0
+    "owner": "airflow",
+    "start_date": dt.datetime(2020, 4, 30, 16, 00, 00),
+    "concurrency": int(Variable.get("concurrency", default_var=1)),
+    "retries": 0,
 }
 
-dag =  DAG('jail_scraper_dag',
-         default_args=default_args,
-         schedule_interval='@once',
-         )
+dag = DAG("jail_scraper_dag", default_args=default_args, schedule_interval="@once",)
 
 
+jail_scraper = PythonIdempatomicFileOperator(
+    task_id="scrape_jail",
+    python_callable=scrape_jail,
+    output_pattern="data/scrapes/{today_date}/jail_scrape/",
+    op_kwargs={"test": True},
+    dag=dag,
+)
 
-jail_scraper = PythonIdempatomicFileOperator(task_id='scrape_jail',
-                                        python_callable=scrape_jail,
-                                        output_pattern = "data/scrapes/{today_date}/jail_scrape/",
-                                        op_kwargs={'test':True},
-                                        dag = dag)
+check_profiles = PythonIdempatomicFileOperator(
+    task_id="check_profiles",
+    python_callable=check_jail_profiles,
+    output_pattern="data/scrapes/{today_date}/to_do/",
+    provide_context=True,
+    dag=dag,
+)
 
-check_profiles = PythonIdempatomicFileOperator(task_id='check_profiles',
-                                            python_callable=check_jail_profiles,
-                                            output_pattern = "data/scrapes/{today_date}/to_do/",
-                                            provide_context=True,
-                                            dag = dag)
-
-upload = PythonOperator(task_id='upload_data',
-                        python_callable=upload_data,
-                        dag=dag)
+upload = PythonOperator(task_id="upload_data", python_callable=upload_data, dag=dag)
 
 
 # pull the variable from Airflow (which was set in previous task)
@@ -165,18 +170,17 @@ upload = PythonOperator(task_id='upload_data',
 # after the dag is instantiated
 num_tasks = int(Variable.get("num_odyssey_scraping_tasks", default_var=1))
 for i in range(num_tasks):
-    odyssey_scraper = PythonIdempatomicFileOperator(task_id='odyssey_scraper_'+str(i),
-                                                    output_pattern = "data/scrapes/{today_date}/worker_{index}/cases.csv",
-                                                    dag = dag,
-                                                    python_callable=scrape_odyssey,
-                                                    provide_context=True,
-                                                    op_kwargs={'index': i})
+    odyssey_scraper = PythonIdempatomicFileOperator(
+        task_id="odyssey_scraper_" + str(i),
+        output_pattern="data/scrapes/{today_date}/worker_{index}/cases.csv",
+        dag=dag,
+        python_callable=scrape_odyssey,
+        provide_context=True,
+        op_kwargs={"index": i},
+    )
 
     check_profiles.set_downstream(odyssey_scraper)
     odyssey_scraper.set_downstream(upload)
 
 
-
-
 jail_scraper >> check_profiles
-
--- airflow/dags/test_dag.py
+++ airflow/dags/test_dag.py
@@ -2,6 +2,7 @@ import unittest
 from airflow.models import DagBag
 from airflow.models import Variable
 
+
 class TestDagIntegrity(unittest.TestCase):
 
     LOAD_SECOND_THRESHOLD = 2
@@ -12,34 +13,41 @@ class TestDagIntegrity(unittest.TestCase):
     def test_import_dags(self):
         self.assertFalse(
             len(self.dagbag.import_errors),
-            'DAG import failures. Errors: {}'.format(
-                self.dagbag.import_errors
-            )
+            "DAG import failures. Errors: {}".format(self.dagbag.import_errors),
         )
+
 
 class TestDagStructure(unittest.TestCase):
     def setUp(self):
         self.dagbag = DagBag()
-        dag_id = 'jail_scraper_dag'
+        dag_id = "jail_scraper_dag"
         self.dag = self.dagbag.get_dag(dag_id)
-        self.current_scale_var = int(Variable.get('num_odyssey_scraping_tasks', default_var=1))
+        self.current_scale_var = int(
+            Variable.get("num_odyssey_scraping_tasks", default_var=1)
+        )
 
     def test_num_tasks(self):
 
-        expected = 3 + self.current_scale_var # 3 single tasks and dynamic scaled task depending on var
+        expected = (
+            3 + self.current_scale_var
+        )  # 3 single tasks and dynamic scaled task depending on var
         self.assertEqual(len(self.dag.tasks), expected)
 
     def test_dependencies_of_check_profiles_task(self):
         """Check the task dependencies of 'check_profiles' in scraper dag"""
-        check_profiles_task = self.dag.get_task('check_profiles')
+        check_profiles_task = self.dag.get_task("check_profiles")
 
-        downstream_task_ids = list(map(lambda task: task.task_id, check_profiles_task.downstream_list))
+        downstream_task_ids = list(
+            map(lambda task: task.task_id, check_profiles_task.downstream_list)
+        )
         downstream_task_ids.sort()
 
-        self.assertListEqual(downstream_task_ids,
-                             [f'odyssey_scraper_{i}'
-                                    for i
-                                    in range(self.current_scale_var)])
+        self.assertListEqual(
+            downstream_task_ids,
+            [f"odyssey_scraper_{i}" for i in range(self.current_scale_var)],
+        )
 
-        upstream_task_ids = list(map(lambda task: task.task_id, check_profiles_task.upstream_list))
-        self.assertListEqual(upstream_task_ids, ['scrape_jail'])
+        upstream_task_ids = list(
+            map(lambda task: task.task_id, check_profiles_task.upstream_list)
+        )
+        self.assertListEqual(upstream_task_ids, ["scrape_jail"])
